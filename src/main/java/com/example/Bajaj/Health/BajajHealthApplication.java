df46320a775aa2bd8a244cc609dcb032989f979d
MaiPriyanshuHoon <911priyanshu@gmail.com>
2025-04-25 17:54:06 +0530

    Initial commit

---
+++ src/main/java/com/example/Bajaj/Health/BajajHealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bajaj.Health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajHealthApplication.class, args);
+	}
+
+}
